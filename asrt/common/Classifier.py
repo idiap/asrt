76eb8c0ddf9b1442724e357f083831fd2dbda556
Alexandre Nanchen <alexandre.nanchen@idiap.ch>
2020-03-04 12:31:00 +0100

    Bug fix.

--- asrt/common/Classifier.py
+++ asrt/common/Classifier.py
@@ -21,7 +21,9 @@ __date__ = "Date: 2012/05/16"
 __copyright__ = "Copyright (c) 2012 Idiap Research Institute"
 __license__ = "BSD 3-Clause"
 
-import string, logging, os
+import string
+import logging
+import os
 import nltk
 
 from nltk.corpus import europarl_raw
@@ -32,18 +34,19 @@ from asrt.config.AsrtConfig import NLTK_DATA
 
 nltk.data.path.append(NLTK_DATA)
 
+
 class LanguageClassifier():
     """An English/French/German/Italian classifier.
     """
     logger = logging.getLogger("Asrt.LanguageClassifier")
 
-    SCORE_THRESHOLD     = 0.03
+    SCORE_THRESHOLD = 0.03
 
     def __init__(self):
         self.classifier = None
         self.scoreDetail = ""
 
-    def classify(self, textUtterance, context = "", removePunctuation = True):
+    def classify(self, textUtterance, context="", removePunctuation=True):
         """Return the class of 'textUtterance'.
 
            Parameters:
@@ -53,21 +56,23 @@ class LanguageClassifier():
         if self.classifier == None:
             raise Exception("Classifier not trained.")
 
-        #Prepare the given text
-        wordsList = self._prepareText(textUtterance, context, removePunctuation)
+        # Prepare the given text
+        wordsList = self._prepareText(
+            textUtterance, context, removePunctuation)
 
-        #Store processing information for debugging
+        # Store processing information for debugging
         self.scoreDetail = ""
 
-        #Hold count results
-        labelCountDict = {FRENCH_LABEL:0, GERMAN_LABEL:0,ITALIAN_LABEL:0,
-                          ENGLISH_LABEL:0}
+        # Hold count results
+        labelCountDict = {FRENCH_LABEL: 0, GERMAN_LABEL: 0, ITALIAN_LABEL: 0,
+                          ENGLISH_LABEL: 0}
 
-        #Label unknown
+        # Label unknown
         for (featuresDict, noLabel) in self.getFeatures(wordsList, None, context):
-            #Classify word features
+            # Classify word features
             label = self.classifier.classify(featuresDict)
-            self.scoreDetail += "%s:  %s\n" % (self.getFeaturesStringRepresentation(featuresDict), label)
+            self.scoreDetail += "%s:  %s\n" % (
+                self.getFeaturesStringRepresentation(featuresDict), label)
             labelCountDict[label] += 1
 
         return self._getResult(labelCountDict)
@@ -75,15 +80,16 @@ class LanguageClassifier():
     def train(self):
         """Train using europarl_raw corpus.
         """
-        nltkDataPath =  os.path.dirname(europarl_raw.french.abspath('ep-00-02-16.fr'))
-        LanguageClassifier.logger.info("Getting features from %s ..." % \
-                                        os.path.dirname(nltkDataPath))
+        nltkDataPath = os.path.dirname(
+            europarl_raw.french.abspath('ep-00-02-16.fr'))
+        LanguageClassifier.logger.info("Getting features from %s ..." %
+                                       os.path.dirname(nltkDataPath))
         train_set = self.getLabelledFeaturesSet()
 
         LanguageClassifier.logger.info("Starting training...")
         self.classifier = nltk.NaiveBayesClassifier.train(train_set)
 
-        #Override trained probabilities
+        # Override trained probabilities
         dist = DictionaryProbDist({FRENCH_LABEL: 0.25, GERMAN_LABEL: 0.25, ITALIAN_LABEL: 0.25,
                                    ENGLISH_LABEL: 0.25})
 
@@ -93,7 +99,7 @@ class LanguageClassifier():
     ########################
     # Getters and setters
     #
-    def getFeatures(self, wordsList, label, context = ""):
+    def getFeatures(self, wordsList, label, context=""):
         """Abstract method to be implemented"""
         pass
 
@@ -111,9 +117,9 @@ class LanguageClassifier():
            ...
         """
         return self._getLabelRawTextFeatures(europarl_raw.french, FRENCH_LABEL) +\
-               self._getLabelRawTextFeatures(europarl_raw.german, GERMAN_LABEL) +\
-               self._getLabelRawTextFeatures(europarl_raw.italian, ITALIAN_LABEL) +\
-               self._getLabelRawTextFeatures(europarl_raw.english, ENGLISH_LABEL)
+            self._getLabelRawTextFeatures(europarl_raw.german, GERMAN_LABEL) +\
+            self._getLabelRawTextFeatures(europarl_raw.italian, ITALIAN_LABEL) +\
+            self._getLabelRawTextFeatures(europarl_raw.english, ENGLISH_LABEL)
 
     def getScoreDetails(self):
         """Detailed information about the score.
@@ -127,18 +133,19 @@ class LanguageClassifier():
         """Raw text with sentences separated by
            spaces.
         """
-        LanguageClassifier.logger.info("Number of sentences: %d for label %s" % (len(language.sents()), label))
+        LanguageClassifier.logger.info(
+            "Number of sentences: %d for label %s" % (len(language.sents()), label))
 
         allFeatures = []
 
         for wordsList in language.sents():
-            #Get all features for the sentence
+            # Get all features for the sentence
             features = self.getFeatures(wordsList, label)
             allFeatures.extend(features)
 
         return allFeatures
 
-    def _prepareText(self, textUtterance, context="", removePunctuation = True):
+    def _prepareText(self, textUtterance, context="", removePunctuation=True):
         """Prepare the text to be normalized:
 
            - remove punctuation
@@ -146,13 +153,14 @@ class LanguageClassifier():
            - remove prepended and trailing spaces
            - decode the string from utf8 to bytes string
         """
-        textUtterance = LanguageClassifier.normalizeText(textUtterance, context, removePunctuation)
+        textUtterance = LanguageClassifier.normalizeText(
+            textUtterance, context, removePunctuation)
         return textUtterance.split(' ')
 
     def _getResult(self, labelCountDict):
         """Return label or unknown.
         """
-        #Get counts
+        # Get counts
         fCount = labelCountDict[FRENCH_LABEL]
         gCount = labelCountDict[GERMAN_LABEL]
         iCount = labelCountDict[ITALIAN_LABEL]
@@ -160,10 +168,10 @@ class LanguageClassifier():
 
         totalCount = fCount + gCount + iCount + eCount
 
-        if totalCount == 0 :
+        if totalCount == 0:
             return (UNKNOWN_LABEL, -1)
 
-        #Compute scores
+        # Compute scores
         fScore = fCount / float(totalCount)
         gScore = gCount / float(totalCount)
         iScore = iCount / float(totalCount)
@@ -171,25 +179,25 @@ class LanguageClassifier():
 
         strScore = "f:%f/g:%f/i:%f/e:%f" % (fScore, gScore, iScore, eScore)
 
-        allScores = [(FRENCH_LABEL,fScore), (GERMAN_LABEL,gScore),
-                     (ITALIAN_LABEL,iScore), (ENGLISH_LABEL,eScore)]
+        allScores = [(FRENCH_LABEL, fScore), (GERMAN_LABEL, gScore),
+                     (ITALIAN_LABEL, iScore), (ENGLISH_LABEL, eScore)]
 
-        #Get max score
+        # Get max score
         maxIndice, maxScore = -1, -1
         for i, (label, score) in enumerate(allScores):
             if score > maxScore:
                 maxIndice = i
                 maxScore = score
 
-        #Validate max score
+        # Validate max score
         noMax = False
         for i, (label, score) in enumerate(allScores):
             if i != maxIndice and\
-                maxScore - score <= LanguageClassifier.SCORE_THRESHOLD:
-                noMax=True
+                    maxScore - score <= LanguageClassifier.SCORE_THRESHOLD:
+                noMax = True
                 break
 
-        #Default to unknown
+        # Default to unknown
         result = (UNKNOWN_LABEL, strScore)
         if not noMax:
             result = allScores[maxIndice]
@@ -200,7 +208,7 @@ class LanguageClassifier():
     # Statics
     #
     @staticmethod
-    def normalizeText(textUtterance, context="", removePunctuation = True):
+    def normalizeText(textUtterance, context="", removePunctuation=True):
         """Normalize text:
 
            - remove punctuation
@@ -208,33 +216,31 @@ class LanguageClassifier():
            - remove prepended and trailing spaces
            - encode the string from utf8 to bytes string
         """
-        if removePunctuation :
+        if removePunctuation:
             textUtterance = LanguageClassifier.removePunctuation(textUtterance)
         textUtterance = textUtterance.lower()
         textUtterance = textUtterance.rstrip()
         textUtterance = textUtterance.strip()
 
-        textUtterance = LanguageClassifier.encodeUtf8String(textUtterance, context)
-
         return textUtterance
 
     @staticmethod
     def removePunctuation(strText, removeHyphen=False,
-                                   removeAppostrophy=False,
-                                   removeDots=True):
+                          removeAppostrophy=False,
+                          removeDots=True):
         excludeString = string.punctuation
 
         if not removeHyphen:
-            excludeString = excludeString.replace('-','')
+            excludeString = excludeString.replace('-', '')
         else:
-            #Need to keep a space
+            # Need to keep a space
             strText = strText.replace('-', ' ')
 
         if not removeAppostrophy:
-            excludeString = excludeString.replace('\'','')
+            excludeString = excludeString.replace('\'', '')
 
         if not removeDots:
-            excludeString = excludeString.replace('.','')
+            excludeString = excludeString.replace('.', '')
 
         exclude = set(excludeString)
 
@@ -246,7 +252,8 @@ class LanguageClassifier():
         try:
             decodedString = strText.decode('utf-8')
         except Exception as e:
-            LanguageClassifier.logger.critical("Decoding error: " + strText + context + str(e))
+            LanguageClassifier.logger.critical(
+                "Decoding error: " + strText + context + str(e))
             raise e
         return decodedString
 
